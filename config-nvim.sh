e49da130ba0ff2d9bd5570e3c466e46cfa6e46fe
Eric Man <ericman@ericman.com>
2021-10-07 19:20:38 -0400

    removes un-needed file

--- config-nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
- https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-ln -s /configs/init.vim ~/.config/nvim/init.vim
